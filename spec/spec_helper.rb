a8941a1ac4628978de5c03d63657103b1fa1ecc2
Steve Hodgkiss <steve@hodgkiss.me.uk>
2011-02-14 22:31:17 +0000

    Don't need to add lib to load path with bundler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'validation_scopes'
 require 'bundler/setup'
+require 'validation_scopes'
 require 'rspec'
 
 RSpec.configure do |config|
